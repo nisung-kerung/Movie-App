7351e2c3273cf8f6a23b67e9192b69d81c712fda
nisung-kerung <nisungkerung@gmail.com>
2025-06-24 13:08:38 +0545

    Update the database

--- Program.cs
+++ Program.cs
@@ -2,8 +2,10 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MvcMovie.Data;
 var builder = WebApplication.CreateBuilder(args);
+// builder.Services.AddDbContext<MvcMovieContext>(options =>
+//     options.UseSqlite(builder.Configuration.GetConnectionString("MvcMovieContext") ?? throw new InvalidOperationException("Connection string 'MvcMovieContext' not found.")));
 builder.Services.AddDbContext<MvcMovieContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("MvcMovieContext") ?? throw new InvalidOperationException("Connection string 'MvcMovieContext' not found.")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MvcMovieContext")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
