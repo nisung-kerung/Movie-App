ca5ca8d9b7aafc830be3a892fbc5b5eb1e1059e4
nisung-kerung <nisungkerung@gmail.com>
2025-06-24 16:37:58 +0545

    add validation to Movie model

--- obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+823297b08632f92a6da5432b787f71ca2b62a0cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bf63d411e74d1c0c7500e922a443c061b82085a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
