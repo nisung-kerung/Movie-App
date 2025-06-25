377dbbf578a9b521a0823072d4f3649d8234cec1
nisung-kerung <nisungkerung@gmail.com>
2025-06-25 06:50:22 +0545

    update the seed data

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -4,56 +4,66 @@ using MvcMovie.Data;
 using System;
 using System.Linq;
 
-namespace MvcMovie.Models;
-
-public static class SeedData
+namespace MvcMovie.Models
 {
-    public static void Initialize(IServiceProvider serviceProvider)
+    public static class SeedData
     {
-        using (var context = new MvcMovieContext(
-            serviceProvider.GetRequiredService<
-                DbContextOptions<MvcMovieContext>>()))
+        public static void Initialize(IServiceProvider serviceProvider)
         {
-            // Look for any movies.
-            if (context.Movie.Any())
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<DbContextOptions<MvcMovieContext>>()))
             {
-                return;   // DB has been seeded
-            }
-            context.Movie.AddRange(
-                new Movie
+                // Check if any movies already exist
+                if (context.Movie.Any())
                 {
-                    Title = "When Harry Met Sally",
-                    ReleaseDate = DateTime.Parse("1989-2-12"),
-                    Genre = "Romantic Comedy",
-                    Rating = "R",
-                    Price = 7.99M
-                },
-                new Movie
-                {
-                    Title = "Ghostbusters ",
-                    ReleaseDate = DateTime.Parse("1984-3-13"),
-                    Genre = "Comedy",
-                    Rating = "R",
-                    Price = 8.99M
-                },
-                new Movie
-                {
-                    Title = "Ghostbusters 2",
-                    ReleaseDate = DateTime.Parse("1986-2-23"),
-                    Genre = "Comedy",
-                    Rating = "R",
-                    Price = 9.99M
-                },
-                new Movie
-                {
-                    Title = "Rio Bravo",
-                    ReleaseDate = DateTime.Parse("1959-4-15"),
-                    Genre = "Western",
-                    Rating = "R",
-                    Price = 3.99M
+                    return; // DB has been seeded
                 }
-            );
-            context.SaveChanges();
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Kabaddi Kabaddi",
+                        ReleaseDate = DateTime.Parse("2015-11-27"),
+                        Genre = "Romantic Comedy",
+                        Rating = "PG",
+                        Price = 5.99M
+                    },
+                    new Movie
+                    {
+                        Title = "Loot",
+                        ReleaseDate = DateTime.Parse("2012-01-13"),
+                        Genre = "Crime",
+                        Rating = "PG-13",
+                        Price = 6.99M
+                    },
+                    new Movie
+                    {
+                        Title = "Pashupati Prasad",
+                        ReleaseDate = DateTime.Parse("2016-01-29"),
+                        Genre = "Drama",
+                        Rating = "PG",
+                        Price = 4.99M
+                    },
+                    new Movie
+                    {
+                        Title = "Kagbeni",
+                        ReleaseDate = DateTime.Parse("2008-01-11"),
+                        Genre = "Horror",
+                        Rating = "PG",
+                        Price = 3.99M
+                    },
+                    new Movie
+                    {
+                        Title = "Chhakka Panja",
+                        ReleaseDate = DateTime.Parse("2016-09-09"),
+                        Genre = "Comedy",
+                        Rating = "U",
+                        Price = 6.49M
+                    }
+                );
+
+                context.SaveChanges();
+            }
         }
     }
 }
